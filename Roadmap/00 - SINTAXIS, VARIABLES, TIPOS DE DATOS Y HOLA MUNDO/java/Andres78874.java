e3f43cd35b13219fdad83b19433f306b60539699
Andres <152053085+Andres78874@users.noreply.github.com>
2024-11-03 14:46:44 -0500

    00#- Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Andres78874.java
@@ -0,0 +1,37 @@
+
+package pkg00.sintaxismoure;
+
+        
+public class Sintaxismoure {
+
+    public static void main(String[] args) {
+    //https://www.java.com/es/    
+    
+    
+    // Con doble barra inclinada podemos hacer comentarios de na linea
+    /* Con barra y inclinada y asterisco podemos hacer comenntarios de varias
+    lineas, cerramos con asterisco y barra inclinada*/ 
+    
+    // variable
+    int i =1;
+    
+    // constante
+    final int num =5;
+    
+    
+    //Tipos primitivos en Java: int, short, long, double, float, boolean, byte, char.
+    int num1 = 65555555;
+    short num2 = 4454;
+    long num3 = 1245852261;
+    double num4 = 125.125445475;
+    float num5 = 125.20f;
+    boolean b = true;
+    byte by =127 ;
+    char a = 'A';
+    
+        System.out.println("Hola, Java");
+    
+
+    }
+    
+}
